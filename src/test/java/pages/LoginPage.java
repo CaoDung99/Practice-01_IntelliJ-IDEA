c6ccff29bf22cf57670b93b15388d315ff97c22b
CaoDung99 <91966835+CaoDung99@users.noreply.github.com>
2022-11-17 09:09:26 +0700

    Viết test case
    Viết bộ test case 1 và 2

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,18 +1,56 @@
 package pages;
 
-import common.BaseTest;
-import io.qameta.allure.Severity;
-import io.qameta.allure.SeverityLevel;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-import pages.LoginPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import supports.WebUI;
 
-public class LoginPage extends  BaseTest{
+public class LoginPage {
+    private WebDriver driver;
+    private WebUI webUI;
 
-    LoginPage loginPage;
-
-    @BeforeClass
-    public void LoginTest(){
-        loginPage = new LoginPage(driver);
+    public LoginPage(WebDriver _driver){
+        driver = _driver;
+        webUI = new WebUI(driver);
     }
+
+    //===============================================================================================
+    By usernameTxt = By.id("username");
+    By passwordTxt = By.id("password");
+    By loginBtn = By.xpath("//button[@type='submit']");
+    By verifyLoginLbl = By.xpath("//div[@id='flash']");
+    By logoutBtn = By.xpath("//a[@class='button secondary radius']");
+
+    //===============================================================================================
+    public void getURL(String url){
+        driver.get(url);
+        webUI.waitForPageLoaded();
+    }
+    public void enterUsername(String username){
+        webUI.setText(usernameTxt, username);
+    }
+    public void enterPassword(String password){
+        webUI.setText(passwordTxt, password);
+    }
+    public void clickLogin(){
+        webUI.clickElement(loginBtn);
+    }
+    public void clickLogout(){
+        webUI.clickElement(logoutBtn);
+    }
+
+    public void verifyLogin(String msg, String option){
+        switch (option) {
+            case "1" -> //happy case
+                    Assert.assertEquals(webUI.getElement(verifyLoginLbl).getText().trim().substring(0, 30), msg.trim());
+            case "2" -> //invalid username
+                    Assert.assertEquals(webUI.getElement(verifyLoginLbl).getText().substring(0, 25), msg.trim());
+            case "3" -> //invalid password
+                    Assert.assertEquals(webUI.getElement(verifyLoginLbl).getText().substring(0, 30), msg.trim());
+            default -> //blank
+                    Assert.assertEquals(webUI.getElement(verifyLoginLbl).getText().substring(0, 30), msg.trim());
+        }
+    }
+
 }
+
